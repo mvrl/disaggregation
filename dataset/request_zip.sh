3e599b11f41f64f6ec21ec55297b40f2495f0733
cgarchbold <cgarchbold@gmail.com>
2020-12-17 16:31:08 -0500

    Deleting Large Notebook

--- dataset/request_zip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-wget 'https://gis.hennepin.us/publicgisdata/hennepin_county_parcels.zip'
-
-unzip hennepin_county_parcels.zip -d hennepin_county_parcels/
-
-rm -f hennepin_county_parcels.zip

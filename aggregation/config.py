3b2c39a363d7ea14c6c8b128812809d9df6e3bdf
Jacob-Birge <jacobbirge24@gmail.com>
2021-08-19 14:10:37 -0400

    preloading values, removal of padding in __getitem__, toggle for Ben's loss method

--- aggregation/config.py
+++ aggregation/config.py
@@ -29,7 +29,7 @@ cfg.train.l2_reg = 1e-6
 cfg.train.lr_decay = 0.9
 cfg.train.lr_decay_every = 3
 cfg.train.shuffle = True                 # shuffle training samples
-cfg.train.num_epochs = 50                # number of training epochs  ...
+cfg.train.num_epochs = 1                # number of training epochs  ...
 cfg.train.num_workers = 8                # workers for data loading
 cfg.train.device_ids = [0,2]                 # Train on two GPUs? Set True for blackbird
 

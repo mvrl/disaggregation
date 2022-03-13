1dec15179b222481fef988ca69a08a0e6a9dd2c2
Aram Ansary Ogholbake <aan244@csr.uky.edu>
2022-03-13 12:15:32 -0400

    train synthetic data

---
+++ synthetic_testing/dataset.py
@@ -0,0 +1,54 @@
+from torch.utils.data import Dataset
+from PIL import Image
+import torch
+import numpy as np
+import os
+import albumentations as A
+from torchvision import transforms
+import random
+import torchvision
+from skimage import io
+from sklearn.model_selection import train_test_split
+import torch
+
+class Eurosat(torch.utils.data.Dataset):
+   
+    
+    def __init__(self, mode='train', root='/localdisk0/SCRATCH/watch/EuroSAT/ds/images/remote_sensing/otherDatasets/sentinel_2/tif/'):
+        data = torchvision.datasets.DatasetFolder(root=root, loader=self.im_loader, transform=None, extensions='tif')
+
+        if mode == 'train':
+            train_set, _ = train_test_split(data, test_size=0.1, stratify=data.targets, random_state=42)
+            self.dset = train_set
+            self.transform = A.Compose([
+               A.HorizontalFlip(p=0.5),
+               A.RandomRotate90(p=1.0),
+            ],
+            )
+        elif mode == 'test' or mode == 'validation':
+            _, val_set = train_test_split(data, test_size=0.1, stratify=data.targets, random_state=42)
+            self.dset = val_set
+            self.transform = A.Compose([
+               A.NoOp()
+            ],
+            )
+
+    def im_loader(self, path):
+   
+        image = np.asarray((io.imread(path)), dtype='float32')
+        image = (image - image.min()) / (image.max()-image.min())
+        return image
+
+    def __len__(self):
+        return len(self.dset)
+
+    def __getitem__(self, idx):
+        item = self.dset.__getitem__(idx)
+
+        transformed = self.transform(image=item[0])
+        image = transformed['image']
+        image = torch.tensor(image).permute(2, 0, 1)
+        label = item[0][:,:, 8]
+        image = image[[2,3,4], :,:]
+        
+        return {'image': image, 'label': label}
---
+++ synthetic_testing/train.py
@@ -0,0 +1,190 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.distributions as dist
+import torchmetrics
+
+import numpy as np
+from argparse import ArgumentParser, Namespace
+from matplotlib import pyplot as plt
+
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor, EarlyStopping
+
+from dataset import Eurosat
+
+from unet import UNet
+
+def gaussLoss(means, vari, targets):
+    losses = []
+    for mean,var,target in zip(means,vari,targets):
+        std = torch.sqrt(var)
+        gauss = dist.Normal(mean, std)
+        losses.append(-torch.sum(gauss.log_prob(target)))
+    loss = torch.stack(losses, dim=0).mean()
+    return loss
+
+class regionize_gauss(pl.LightningModule):
+    def __init__(self, hparams):
+        super().__init__()
+       
+        if type(hparams)==dict:
+            hparams = Namespace(**hparams)
+        
+        self.unet = UNet(3, 2) 
+        self.softplus = nn.Softplus()
+        self.avg_pool = nn.AvgPool2d((hparams.kernel_size,hparams.kernel_size), stride=hparams.kernel_size)
+        self.flatten =  nn.Flatten()
+                                      
+        self.trainset = Eurosat(mode='train')
+        self.valset = Eurosat(mode='validation') 
+        self.save_hyperparameters(hparams)
+
+    def forward(self, x):
+        
+        x = self.unet(x)
+
+        mean = x[:, 0]
+        var = x[:, 1]
+
+        var = self.softplus(var)
+
+        agg_mean = self.avg_pool(mean) * self.hparams.kernel_size**2
+        agg_var = self.avg_pool(var) * self.hparams.kernel_size**2
+
+        agg_mean = self.flatten(agg_mean)
+        agg_var = self.flatten(agg_var)
+        
+        
+        return  agg_mean, agg_var
+               
+    
+    def pred_Out(self, x):
+        
+        means, _ = self (x)
+        
+        return means
+    
+
+    def log_out(self, x, targets):
+        
+        means, vari = self(x)
+
+        losses=[]
+        for mean,var,target in zip(means,vari, targets):
+            std = torch.sqrt(var)
+            gauss = dist.Normal(mean, std)
+            losses.append(torch.exp(gauss.log_prob(target)))
+        return losses
+    
+    def agg_labels(self, labels):
+        
+        labels = self.avg_pool(labels) * self.hparams.kernel_size**2
+        agg_labels = self.flatten(labels)
+        
+        return agg_labels
+
+
+    def shared_step(self, batch):
+        
+        images = batch['image']
+        labels = batch['label']
+        
+        agg_labels = self.avg_pool(labels) * self.hparams.kernel_size**2
+        agg_labels = self.flatten(agg_labels)
+        
+        
+        means, var = self (images)
+
+        print (means, "means")
+        print (var, "var")
+        print (agg_labels, "labels")
+        
+        loss = gaussLoss(means, var, agg_labels)
+                
+        return {'loss': loss}
+    
+    def training_step(self, batch, batch_idx):
+        
+        output = self.shared_step(batch)
+        self.log('train_loss', output['loss'],  
+                on_epoch = True, batch_size=self.hparams.batch_size)
+        return output['loss']
+    
+    
+    def validation_step(self, batch, batch_idx):
+        
+        output = self.shared_step(batch)
+        self.log('val_loss', output['loss'])
+        return output['loss']
+
+
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(self.trainset, 
+                batch_size = self.hparams.batch_size,
+                num_workers = self.hparams.workers,
+                shuffle=True,
+                pin_memory=False
+                )
+
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(self.valset, 
+                batch_size = 1,
+                num_workers = self.hparams.workers,
+                pin_memory=False
+                )
+    
+    def configure_optimizers(self):
+        
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+        
+        return {'optimizer': optimizer}
+ 
+def main(args):
+                             
+    if type(args)==dict:
+            args = Namespace(**args)
+    
+
+    log_dir = '{}'.format(
+        args.save_dir,
+        )
+
+    logger = TensorBoardLogger(log_dir)
+    
+    
+    model = regionize_gauss(hparams=args)
+
+    checkpoint_callback = ModelCheckpoint(monitor='val_loss', mode='min', save_top_k=3, save_last=True)
+    
+    early_stopping = EarlyStopping('val_loss', patience=args.patience)
+
+    trainer = pl.Trainer.from_argparse_args(args,
+                                            max_epochs = args.max_epochs,
+                                            logger=logger, 
+                                            callbacks=[checkpoint_callback, early_stopping],
+                                            )
+    trainer.fit(model)    
+    
+if __name__ == '__main__':
+    from argparse import ArgumentParser, Namespace 
+    
+    parser = ArgumentParser()
+    
+    parser.add_argument('--max_epochs', type=int, default=100)
+    parser.add_argument('--workers', type=int, default=8)
+    parser.add_argument('--batch_size', type=int, default=4)
+    parser.add_argument('--step_size', type=int, default=40)
+    parser.add_argument('--learning_rate', type=float, default=.0001)
+    parser.add_argument('--save_dir', default='logs')
+    parser.add_argument('--gpus', type=int, default=1)
+    parser.add_argument('--drop_rate', type=float, default=.1)
+    parser.add_argument('--kernel_size', type=int, default=16)
+    parser.add_argument('--patience', type=int, default=100)
+    parser.add_argument('--num_regions', type=int, default=10)
+                         
+    
+    args = parser.parse_args() 
+    main(args)

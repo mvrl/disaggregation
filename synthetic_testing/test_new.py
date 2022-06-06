57d70f6d5a5b2cb415542e44d153878bccda562e
Aram Ansary Ogholbake <aan244@csr.uky.edu>
2022-06-06 09:43:52 -0400

    add deterministic model

--- synthetic_testing/test_new.py
+++ synthetic_testing/test_new.py
@@ -6,7 +6,7 @@ import train_new as train
 import torch.distributions as dist
 import matplotlib.pyplot as plt
 import numpy as np
-
+from PIL import Image
 
 testset = Eurosat(mode='test')
 
@@ -28,8 +28,12 @@ def generate_pred_lists(model, dir_path, method):
     std_arr = []
     value_arr = []
     logs = []
-    ros = []
-    # i= 0
+    ros15 = []
+    ros25 = []
+    ros35 = []
+    ros50 = []
+    ros1 = []
+    i= 0
     with torch.no_grad():
         for sample in tqdm(test_loader):
             images = sample['image']
@@ -38,40 +42,55 @@ def generate_pred_lists(model, dir_path, method):
             labels = torch.flatten(labels, 1, 2)
 
             estimated_values, std_values = model.pred_Out(images)
+           # estimated_values  = model.pred_Out(images)
 
             estimated_arr.extend(estimated_values[0].cpu().numpy().tolist())
             std_arr.extend(std_values[0].cpu().numpy().tolist())
             value_arr.extend(labels[0].cpu().numpy().tolist())
 
-            log = train.gaussLoss_test(estimated_values, std_values, labels).cpu().numpy().tolist()
+            log = gaussLoss_test(estimated_values, std_values, labels).cpu().numpy().tolist()
             print(log)
             logs.append(log)
 
             gauss = dist.Normal(estimated_values, std_values)
 
-            ro = ((gauss.cdf(labels + 5.) - gauss.cdf(labels - 5.))[0].cpu().numpy().tolist())
-            ros.extend(ro)
+            ro15 = ((gauss.cdf(labels + 0.05) - gauss.cdf(labels - 0.05))[0].cpu().numpy().tolist())
+            ro25 = ((gauss.cdf(labels + 0.1) - gauss.cdf(labels - 0.1))[0].cpu().numpy().tolist())
+            ro35 = ((gauss.cdf(labels + 0.35) - gauss.cdf(labels - 0.35))[0].cpu().numpy().tolist())
+            ro50 = ((gauss.cdf(labels + 0.5) - gauss.cdf(labels - 0.5))[0].cpu().numpy().tolist())
+            ro1 = ((gauss.cdf(labels + 1.) - gauss.cdf(labels - 1.))[0].cpu().numpy().tolist())
+            ros15.extend(ro15)
+            ros25.extend(ro25)
+            ros35.extend(ro35)
+            ros50.extend(ro50)
+            ros1.extend(ro1)
+            
 
-    # plt.imsave( "label"+str(i)+".png", (torch.tensor(labels.squeeze())).cpu())
-    # plt.imsave("mean"+str(i)+".png",(torch.tensor((estimated_values.squeeze()))).cpu())
-    # i += 1
+           # print (labels.shape, labels.squeeze().shape)
+           # print (estimated_values.shape, estimated_values.squeeze().shape)
+           # print (images.shape, estimated_values.squeeze().shape)
+            i += 1
 
 
     mse_errors = np.square(np.array(value_arr) - np.array(estimated_arr))
     log_error = (np.array(logs)).mean()
-    ro_mean = (np.array(ros)).mean()
+    ro15_mean = (np.array(ros15)).mean()
+    ro25_mean = (np.array(ros25)).mean()
+    ro35_mean = (np.array(ros35)).mean()
+    ro50_mean = (np.array(ros50)).mean()
+    ro1_mean = (np.array(ros1)).mean()
     print(np.array(std_arr).mean(), "std")
     
     value_arr = np.array(value_arr)
-    gaussian_MSE = ( value_arr - np.array(model.gauss_fit()[0]))**2
-    print (gaussian_MSE.mean(), "gauss_mse")
-    Gauss = model.gauss_fit()[2]
-    log_gaussian = (Gauss.log_prob (torch.tensor(value_arr))).mean()
-    print (log_gaussian,"gauss_log")
-    print (model.gauss_fit()[1], "gauss_std")
-    print (torch.mean(Gauss.cdf(torch.tensor(value_arr) + torch.tensor(5.)) - Gauss.cdf(torch.tensor(value_arr) - torch.tensor(5.))))
-    print (torch.mean(Gauss.cdf(torch.tensor(value_arr) + torch.tensor(1.)) - Gauss.cdf(torch.tensor(value_arr) - torch.tensor(1.))))
-    return mse_errors.mean(), log_error, ro_mean
+    #gaussian_MSE = ( value_arr - np.array(model.gauss_fit()[0]))**2
+    #print (gaussian_MSE.mean(), "gauss_mse")
+    #Gauss = model.gauss_fit()[2]
+    #log_gaussian = (Gauss.log_prob (torch.tensor(value_arr))).mean()
+    #print (log_gaussian,"gauss_log")
+    #print (model.gauss_fit()[1], "gauss_std")
+    #print (torch.mean(Gauss.cdf(torch.tensor(value_arr) + torch.tensor(0.5)) - Gauss.cdf(torch.tensor(value_arr) - torch.tensor(0.5))))
+    #print (torch.mean(Gauss.cdf(torch.tensor(value_arr) + torch.tensor(0.15)) - Gauss.cdf(torch.tensor(value_arr) - torch.tensor(0.15))))
+    return mse_errors.mean(), log_error, ro15_mean,ro25_mean,ro35_mean,ro50_mean,ro1_mean,np.array(std_arr).mean()
 
 
 def main(args):
@@ -82,7 +101,7 @@ def main(args):
 
     # use the desired check point path
     ckpt_path = os.path.join(dir_path,
-                             '80/logtest/uniform/16/10/default/version_19/checkpoints/epoch=47-step=9119.ckpt')
+                             '80/logtest/analytical/16/10/default/version_18/checkpoints/epoch=66-step=12729.ckpt')
     torch.cuda.set_device(1)
     if args.method == 'analytical':
         model = train.AnalyticalRegionAggregator(args)
@@ -92,12 +111,17 @@ def main(args):
         model = train.SamplingRegionAggregator(10, args)
         model = model.load_from_checkpoint(ckpt_path,
                                            use_pretrained=False, k=10).eval()
+    elif args.method == 'uniform':
+        model = train.Uniform_model( args)
+        model = model.load_from_checkpoint(ckpt_path,
+                                           use_pretrained=False).eval()
 
-    mae_error, log_error, ro = generate_pred_lists(model, dir_path, args.method)
+    mse_error, log_error, ro15,ro25,ro35,ro50,ro1,std = generate_pred_lists(model, dir_path, args.method)
 
     test_file = open(test_file_path, "a")
-    L = ["\nStats 10: " + str(args.method), "\nMSE: " + str(mae_error),
-         "\nAverage Log Prob: " + str(log_error), "\nro: " + str(ro)]
+    L = ["\nStats 10: " + str(args.method), "\nMSE: " + str(mse_error),
+         "\nAverage Log Prob: " + str(log_error), "\nro15: " + str(ro15),"\nro25: " + str(ro25), 
+         "\nro35: " + str(ro35),"\nro50: " + str(ro50),"\nro1: " + str(ro1),"\nstd: " + str(std)]
 
     test_file.writelines(L)
 
--- synthetic_testing/train_new.py
+++ synthetic_testing/train_new.py
@@ -10,18 +10,19 @@ from argparse import ArgumentParser, Namespace
 from pytorch_lightning import seed_everything
 import test_new 
 
+
+def MSE(outputs, targets):
+    
+    losses = (outputs - targets)**2
+    losses = (torch.sum(losses, 1))
+    losses = torch.mean(losses)
+    
+    return losses
+
 def gaussLoss_train(mean, std, target):
     gauss = dist.Normal(mean, std)
     loss = gauss.log_prob(target)
     loss = -(torch.sum(loss, 1))
-    loss = torch.mean(loss)
-    return loss
-
-
-def gaussLoss_test(mean, std, target):
-    gauss = dist.Normal(mean, std)
-    loss = gauss.log_prob(target)
-    loss = -(torch.mean(loss, 1))
     loss = torch.mean(loss)
     return loss
 
@@ -54,7 +55,7 @@ class RegionAggregator(pl.LightningModule):
         images = batch['image']
         labels = batch['label']
 
-        log_prob_orig = -gaussLoss_test(self.pred_Out(images)[0], 
+        log_prob_orig = -test_new.gaussLoss_test(self.pred_Out(images)[0], 
                                         self.pred_Out(images)[1],
                                         self.flatten(labels))
         
@@ -64,7 +65,7 @@ class RegionAggregator(pl.LightningModule):
         
         
         mean_std = torch.mean(std)
-        log_prob = -gaussLoss_test(mu, std, labels)
+        log_prob = -test_new.gaussLoss_test(mu, std, labels)
         
 
         loss = gaussLoss_train(mu, std, labels)
@@ -117,7 +118,7 @@ class RegionAggregator(pl.LightningModule):
         data_x = []
         
         for i in range(self.trainset.__len__()):
-            data_y.append(self.avg_pool(self.trainset.__getitem__(i)['label'].unsqueeze(0)).numpy()*(self.hparams.kernel_size**2))
+            data_y.append(self.trainset.__getitem__(i)['label'].unsqueeze(0).numpy())
         mean, std= scipy.stats.norm.fit( data_y)
         gauss = torch.distributions.Normal(mean,std)
         return mean, std, gauss
@@ -195,8 +196,8 @@ class Uniform_model(pl.LightningModule):
     def forward(self, x):
         x = self.unet(x)
 
-        mean = x[:, 0]
-        std = x[:, 1]
+        mean = x[:, 0]  
+        std = x[:, 1] 
         std = self.softplus(std) + 1e-16
 
         return mean, std
@@ -263,6 +264,85 @@ class Uniform_model(pl.LightningModule):
         return means, std
 
 
+
+class deterministic_model(pl.LightningModule):
+    def __init__(self, hparams):
+        super().__init__()
+
+        if type(hparams) == dict:
+            hparams = Namespace(**hparams)
+
+        self.unet = UNet(3, 1)
+        self.softplus = nn.Softplus()
+        self.avg_pool = nn.AvgPool2d((hparams.kernel_size, hparams.kernel_size), stride=hparams.kernel_size)
+        self.flatten = nn.Flatten()
+        self.trainset = Eurosat(mode='train')
+        self.valset = Eurosat(mode='validation')
+        self.save_hyperparameters(hparams)
+
+    def forward(self, x):
+        x = self.unet(x)
+
+        return x
+
+    def shared_step(self, batch):
+        images = batch['image']
+        labels = batch['label']
+
+       # log_prob_orig = -gaussLoss_test(self.pred_Out(images)[0],
+       #                                 self.pred_Out(images)[1],
+       #                                 self.flatten(labels))
+
+        labels = self.avg_pool(labels)*self.hparams.kernel_size**2
+        labels = self.flatten(labels)
+
+        net_out  = self(images)
+        net_out = self.avg_pool(net_out)*self.hparams.kernel_size**2
+        net_out = self.flatten(net_out)
+
+        loss = MSE(net_out,labels)
+
+        return {'loss': loss}
+
+
+    def training_step(self, batch, batch_idx):
+        output = self.shared_step(batch)
+
+        self.log('train_loss', output['loss'])
+        return output['loss']
+
+    def validation_step(self, batch, batch_idx):
+        output = self.shared_step(batch)
+        self.log('val_loss', output['loss'])
+        return output['loss']
+
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(self.trainset,
+                                           batch_size=self.hparams.batch_size,
+                                           num_workers=self.hparams.workers,
+                                           shuffle=True,
+                                           pin_memory=False
+                                           )
+
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(self.valset,
+                                           batch_size=self.hparams.batch_size,
+                                           num_workers=self.hparams.workers,
+                                           pin_memory=False
+                                           )
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+
+        return {'optimizer': optimizer}
+
+    
+    def pred_Out(self, x):
+        net_out = self(x)
+        net_out = self.flatten (net_out)
+        return net_out
+
+
 def main(args):
     if type(args) == dict:
         args = Namespace(**args)
@@ -285,6 +365,8 @@ def main(args):
         model = SamplingRegionAggregator(args.samples, args)
     elif args.method == 'uniform':
         model = Uniform_model(args)
+    elif args.method == 'det':
+        model = deterministic_model(args)
     
     checkpoint_callback = ModelCheckpoint(monitor='val_loss', mode='min', save_top_k=1)
 
@@ -302,7 +384,7 @@ def main(args):
 if __name__ == '__main__':
 
     parser = ArgumentParser()
-    parser.add_argument('--max_epochs', type=int, default=50)
+    parser.add_argument('--max_epochs', type=int, default=150)
     parser.add_argument('--workers', type=int, default=16)
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--learning_rate', type=float, default=.01)
